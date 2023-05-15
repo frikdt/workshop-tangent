20c043d3e985d909902f1ece6e4fd81c2a861cc7
frikdt <frikdt@akira-0919>
2023-05-15 11:58:57 +0200

    - Added terraform configuration files.

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,36 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Environment = var.tag_env
+    Team = var.tag_team
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true
+  tags = {
+    Environment = var.tag_env
+    Team = var.tag_team
+  }
+}
